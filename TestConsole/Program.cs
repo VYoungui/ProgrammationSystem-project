196a086aa8a2353374e2b75044b4297d8f14c7cf
stracksemiPwr <remi.pouamo@2026.ucac-icam.com>
2023-12-21 04:45:45 +0100

    kitchen

---
+++ TestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using controller.kitchen;
+using model.kitchen;
+using model.kitchen.clientOrder;
+using model.kitchen.kitchenStaff;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        KitchenModel kitchenModel = new KitchenModel();
+        KitchenController kitchenController = new KitchenController(kitchenModel);
+        kitchenController.play();
+
+
+    }
+}
